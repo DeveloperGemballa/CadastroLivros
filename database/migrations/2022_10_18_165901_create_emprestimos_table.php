fd81ed0bb7a656c0e24e01d83c84a537741a7899
DeveloperGemballa <matheus.gullini@unidavi.edu.br>
2022-10-19 13:41:04 -0300

    a

---
+++ database/migrations/2022_10_18_165901_create_emprestimos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('emprestimos', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger("contatos_id");
+            $table->foreign("contato_id")->references('id')->on("contatos");
+            $table->unsignedBigInteger("livros_id")->unsi;
+            $table->foreign("livro_id")->references('id')->on("livros_laravel");
+            $table->dateTime('datahora');
+            $table->dateTime('datadevolucao');
+            $table->text('obs');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('emprestimos');
+    }
+};
